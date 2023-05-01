65e6806c89a32513ecd16000d19efdd95cc04065
wyantandrew <131827926+wyantandrew@users.noreply.github.com>
2023-05-01 16:59:58 -0400

    trying to add play again loop

--- numberGuessingGameScratch.py
+++ numberGuessingGameScratch.py
@@ -17,28 +17,54 @@ print(correct_num)
       
 func_start_game()
 
-try:
-    input_guessed_num = int(input("Please guess a number 0-50: "))
-    func_increase_list_count()
-except ValueError:
-    print("numbers only")    
-
-while input_guessed_num != correct_num:
+while True:
     try:
+        input_guessed_num = int(input("Please guess a number 0 - 50: "))
+        if input_guessed_num > 50:
+            print("Must guess 0 - 50\n")
+            continue
+        elif input_guessed_num < 0:
+            print("Must guess 0 - 50\n")        
+            continue
         if input_guessed_num < correct_num:
-            print("It's higher\n")
-            input_guessed_num = int(input("Please guess a number 0-50: "))
+            print("It's higher.\n")
             func_increase_list_count()
-        
+            continue
         elif input_guessed_num > correct_num:
             print("It's lower.\n")
-            input_guessed_num = int(input("Please guess a number 0-50: "))
             func_increase_list_count()
+            continue
+        elif input_guessed_num == correct_num:
+            break
     except ValueError:
-        print("Numbers only please.") 
+        print("Numnbers only please.\n") 
+        continue       
 
-            
-print(f"Got it in {len(list_count)} attempts \nGreat job!\n")
+print(f"Got it in {len(list_count) + 1} attempts \nGreat job!\n")
+input_play_again = input("Play again? Y/N: ")
+
+while input_play_again.upper == "Y":
+    try:
+        input_guessed_num = int(input("Please guess a number 0 - 50: "))
+        if input_guessed_num > 50:
+            print("Must guess 0 - 50\n")
+            continue
+        elif input_guessed_num < 0:
+            print("Must guess 0 - 50\n")        
+            continue
+        if input_guessed_num < correct_num:
+            print("It's higher.\n")
+            func_increase_list_count()
+            continue
+        elif input_guessed_num > correct_num:
+            print("It's lower.\n")
+            func_increase_list_count()
+            continue
+        elif input_guessed_num == correct_num:
+            break
+    except ValueError:
+        print("Numnbers only please.\n") 
+        continue       
 
 
 
