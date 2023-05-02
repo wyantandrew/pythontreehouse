3948a7747baff88e7db826a64999497504af9a37
wyantandrew <131827926+wyantandrew@users.noreply.github.com>
2023-05-01 20:49:57 -0400

    Update numberGuessingGameB.py

---
+++ numberGuessingGameB.py
@@ -0,0 +1,46 @@
+
+
+list_count = []
+
+import random
+
+correct_num = random.randrange(51)
+print(correct_num)
+
+def func_increase_list_count(input_guessed_num):
+    list_count.append(input_guessed_num)
+
+def func_start_game():
+    while True:
+        try:
+            input_guessed_num = int(input("Please guess a number 0 - 50: "))
+            if input_guessed_num > 50:
+                print("Must guess 0 - 50\n")
+                continue
+            elif input_guessed_num < 0:
+                print("Must guess 0 - 50\n")        
+                continue
+            if input_guessed_num < correct_num:
+                print("It's higher.\n")
+                func_increase_list_count(input_guessed_num)
+                continue
+            elif input_guessed_num > correct_num:
+                print("It's lower.\n")
+                func_increase_list_count(input_guessed_num)
+                continue
+            elif input_guessed_num == correct_num:
+                break
+        except ValueError:
+            print("Numnbers only please.\n") 
+            continue     
+
+print("***Welcome to the Number Guessiing Game*** \n")
+
+func_start_game()
+
+print(f"Got it in {len(list_count) + 1} valid attempts \nGreat job! Bye!\n")
+
+    
+
+
+
